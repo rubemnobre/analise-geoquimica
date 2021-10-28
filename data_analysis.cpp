a43578c913b2bd5389f1d893b5ff18871a73271d
Rubem Nobre <rubemjrx@hotmail.com>
2021-10-28 07:41:46 -0300

    delegar saida a funcoes de classe

--- data_analysis.cpp
+++ data_analysis.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <iostream>
 #include <stdexcept>
+#include <iomanip>
 
 std::vector<std::string> data::get_classes(data::component_vector components){
     auto compare_by_cls = [](data::chemical_component a, data::chemical_component b) {return a.cls < b.cls;};
@@ -49,7 +50,7 @@ data::heteroatomic_class::heteroatomic_class(data::component_vector components, 
 
 data::DBE::DBE(data::component_vector components, int dbe){
     dbe_components = components;
-
+    if(components.size() > 0) parent_class = components[0].cls;
     val = dbe;
 
     // Ordena os componentes do DBE por número de carbono
@@ -164,4 +165,81 @@ float data::heteroatomic_class::sum_C(int dbe, int min, int max, int opt){
 
 float data::heteroatomic_class::sum_C(int dbe, std::vector<int> vals){
     return get_DBE(dbe)->sum_c(vals);
+}
+
+void data::sample::print_relative_abundancy(std::ostream &output){
+    output << std::fixed << std::setprecision(6);
+    output << "\nAbundancia das classes heteroatomicas\n";
+    output << "Intensidade total: " << intensity << "\n";
+    output << "Classe\tInt. Relativa\n";
+    for(auto i : classes){
+        output << i.first << '\t' << i.second->intensity/intensity << "\n";
+    }
+
+    output << '\n';
+}
+
+void data::sample::print_biodegradation(std::ostream &output){
+    auto O2 = *get_class("O2");
+    auto O1 = *get_class("O1");
+    output << "Indices de biodegradacao:\n";
+    output << "A/C: " << (O2.get_DBE(1)->intensity)/O2.sum_dbe(2, 4) << "\n";
+    output << "A/C mod.: " << (O2.get_DBE(1)->intensity)/O2.sum_dbe(2, 6) << "\n";
+    output << "Indice S/A: " << O2.sum_dbe(1, 6) << "\n";
+    output << "S/A mod.: " << O2.sum_dbe(2, 6) << "\n";
+    output << "Indice MA1: " << (O1.get_DBE(4)->intensity)/(O1.get_DBE(5)->intensity) << "\n";
+    output << "Indice MA2: " << (O1.get_DBE(4)->intensity)/(O1.get_DBE(7)->intensity) << "\n";
+}
+
+void data::sample::print_paleoenvironment(std::ostream &output){
+    auto O2 = *get_class("O2");
+    auto O1 = *get_class("O1");
+    auto NO = *get_class("NO");
+    auto N = *get_class("N");
+
+    output << "Indices de paleoambiente:\n";
+    output << "Indice Phenol: " << (O1.get_DBE(4)->intensity)/O1.intensity << "\n";
+    output << "C27/C28 (DBE4): " << (O1.C(4, 27))/(O1.C(4, 28)) << "\n";
+    output << "C27/C28 (DBE5): " << (O1.C(5, 27))/(O1.C(5, 28)) << "\n";
+    output << "Par/Impar (FA): " << (O2.sum_C(1, 20, 36, data::DBE::even))/(O2.sum_C(1, 19, 35, data::DBE::odd)) << "\n";
+    output << "OEP (FA): " << (O2.C(1, 22) + 6.0*O2.C(1, 28) + O2.C(1, 26))/(4*O2.C(1, 23) + 4*O2.C(1, 25)) << "\n";
+    output << "TAR (FA) Par: " << (O2.sum_C(1, {24, 26, 28}))/(O2.sum_C(1, {12, 14, 16})) << "\n";
+    output << "TAR (FA) Impar: " << (O2.sum_C(1, {27, 29, 31}))/(O2.sum_C(1, {15, 17, 19})) << "\n";
+    output << "C36 Indice de Acido Hepanoico: " << O2.C(6, 36)/O2.C(6, 31) << "\n";
+    output << "Hopano / Acido Estanoico: " << O2.C(6, 31)/O2.C(5, 28) << "\n";
+    output << "Razao Rocha 1: " << O2.sum_dbe(1, 7)/(O2.sum_dbe(1, 7) + O2.sum_dbe(8, 25)) << "\n";
+    output << "Razao Rocha 2: " << O1.sum_dbe(4, 10)/(O1.sum_dbe(4, 10) + O1.sum_dbe(11, 23)) << "\n";
+    output << "Razao Rocha 3: " << NO.get_DBE(10)->intensity << "\n";
+    output << "Razao Rocha 4: " << O1.get_DBE(4)->intensity << "\n";
+    output << "Razao Rocha 5: " << N.sum_dbe(6, 10)/(N.sum_dbe(6, 10) + N.sum_dbe(10, 27)) << "\n";
+    output << "Razao Rocha 6: " << N.sum_dbe(2, 14)/(N.sum_dbe(2, 14) + N.sum_dbe(15, 29)) << "\n";
+    output << "Razao Rocha 7: " << O1.sum_dbe(4, 12) + O2.get_DBE(12)->intensity/(O1.sum_dbe({4, 12, 14, 16}) + O2.sum_dbe({12, 13})) << "\n";
+    output << "Razao Rocha 8: " << N.sum_dbe({8, 9}) + NO.sum_dbe({9, 10})/(N.sum_dbe({8, 9, 13, 16}) + NO.sum_dbe({9, 10, 19, 20})) << "\n";
+}
+
+void data::sample::print_maturity(std::ostream &output){
+    auto N = *get_class("N");
+
+    output << "Indices de maturidade:\n";
+    output << "DBE9  da Classe N: " << N.get_DBE(9)->intensity << "\n";
+    output << "DBE12 da Classe N: " << N.get_DBE(12)->intensity << "\n";
+    output << "DBE15 da Classe N: " << N.get_DBE(15)->intensity << "\n";
+}
+
+void data::heteroatomic_class::print_intensity_per_dbe(std::ostream &output){
+    output << "\nAbundancia relativa do DBE na Classe Heteroatomica " << class_name;
+    output << "\nIntensidade total da classe: " << intensity << "\nDBE\tInt. Relativa\n";
+    for(auto i : class_dbes){
+        output     << i.first << '\t' << i.second->intensity/intensity << '\n';
+    }
+    output << '\n';
+}
+
+void data::DBE::print_intensity_per_c(std::ostream &output){
+    output << "Distribuicao da quantidade de carbonos no DBE" << val << " da classe" << parent_class << '\n';
+    output << "Num. C\tIntensidade\n";
+    for(int i = min_c; i <= max_c; i++){
+        output << i << '\t' << c_intensity[i].x << '\n';
+    }
+    output << '\n';
 }
--- main.cpp
+++ main.cpp
@@ -8,23 +8,12 @@
 #include <algorithm>
 #include "input_data.hpp"
 
-void relative_abundancy(data::sample input, std::ofstream *file){
-    *file << std::fixed << std::setprecision(6);
-    *file << "\nAbundancia das classes heteroatomicas\n";
-    *file << "Intensidade total: " << input.intensity << "\n";
-    *file << "Classe\tInt. Relativa\n";
-    std::cout << "\nIntensidade total: " << input.intensity << "\n";
-    std::cout << "Classe\tInt. Relativa\n";
-    for(auto i : input.classes){
-        *file << i.first << '\t' << i.second->intensity/input.intensity << "\n";
-        std::cout << i.first << '\t' << i.second->intensity/input.intensity << "\n";
-    }
-
-    *file << '\n';
-    std::cout << '\n';
+void relative_abundancy(data::sample input, std::ofstream &file){
+    input.print_relative_abundancy(std::cout);
+    input.print_relative_abundancy(file);
 }
 
-void petrochemical_study(data::sample input, std::ofstream *file){
+void petrochemical_study(data::sample input, std::ofstream &file){
     int choice = 0;
     std::cout << "1. Calcular abundancia relativa do DBE por classe\n2. Calcular distribuicao de numeros carbono por DBE";
     std::cout << "\nEscolha: ";
@@ -43,14 +32,8 @@ void petrochemical_study(data::sample input, std::ofstream *file){
         
         auto hclass = *input.classes[input.class_names[i_sel]];
         
-        std::cout << "\nIntensidade total da classe: " << hclass.class_name << "\nDBE\tInt. Relativa\n";
-        *file << "\nAbundancia relativa do DBE na Classe Heteroatomica " << hclass.class_name;
-        *file << "\nIntensidade total da classe: " << hclass.intensity << "\nDBE\tInt. Relativa\n";
-        for(auto i : hclass.class_dbes){
-            std::cout << i.first << '\t' << i.second->intensity/hclass.intensity << '\n';
-            *file     << i.first << '\t' << i.second->intensity/hclass.intensity << '\n';
-        }
-        *file << '\n';
+        hclass.print_intensity_per_dbe(std::cout);
+        hclass.print_intensity_per_dbe(file);
     }
     if(choice == 2){
         int n = input.class_names.size();
@@ -72,20 +55,12 @@ void petrochemical_study(data::sample input, std::ofstream *file){
 
         auto dbe = *hclass.class_dbes[dbe_sel];
 
-        std::cout << "\nDistribuicao da quantidade de carbonos no DBE" << dbe_sel << " da classe" << hclass.class_name << '\n';
-        *file << "Distribuicao da quantidade de carbonos no DBE" << dbe_sel << " da classe" << hclass.class_name << '\n';
-        std::cout << "Num. C\tIntensidade\n";
-        *file << "Num. C\tIntensidade\n";
-        for(int i = dbe.min_c; i <= dbe.max_c; i++){
-            std::cout << i << '\t' << dbe.c_intensity[i].x << '\n';
-            *file << i << '\t' << dbe.c_intensity[i].x << '\n';
-        }
-        *file << '\n';
-        std::cout << '\n';
+        dbe.print_intensity_per_c(std::cout);
+        dbe.print_intensity_per_c(file);
     }
 }
 
-void geochemical_study(data::sample input, std::ofstream *file){
+void geochemical_study(data::sample input, std::ofstream &file){
     std::cout << std::fixed << std::setprecision(6);
     char opt = '0';
     while(opt != '4'){
@@ -95,44 +70,16 @@ void geochemical_study(data::sample input, std::ofstream *file){
                      "4. Sair\nDigite a escolha: ";
         std::cin >> opt;
         if(opt == '1'){
-            auto O2 = *input.get_class("O2");
-            auto O1 = *input.get_class("O1");
-            std::cout << "A/C: " << (O2.get_DBE(1)->intensity)/O2.sum_dbe(2, 4) << "\n";
-            std::cout << "A/C mod.: " << (O2.get_DBE(1)->intensity)/O2.sum_dbe(2, 6) << "\n";
-            std::cout << "Indice S/A: " << O2.sum_dbe(1, 6) << "\n";
-            std::cout << "S/A mod.: " << O2.sum_dbe(2, 6) << "\n";
-            std::cout << "Indice MA1: " << (O1.get_DBE(4)->intensity)/(O1.get_DBE(5)->intensity) << "\n";
-            std::cout << "Indice MA2: " << (O1.get_DBE(4)->intensity)/(O1.get_DBE(7)->intensity) << "\n";
+            input.print_biodegradation(std::cout);
+            input.print_biodegradation(file);
         }
         if(opt == '2'){
-            auto O2 = *input.get_class("O2");
-            auto O1 = *input.get_class("O1");
-            auto NO = *input.get_class("NO");
-            auto N = *input.get_class("N");
-
-            std::cout << "Indice Phenol: " << (O1.get_DBE(4)->intensity)/O1.intensity << "\n";
-            std::cout << "C27/C28 (DBE4): " << (O1.C(4, 27))/(O1.C(4, 28)) << "\n";
-            std::cout << "C27/C28 (DBE5): " << (O1.C(5, 27))/(O1.C(5, 28)) << "\n";
-            std::cout << "Par/Impar (FA): " << (O2.sum_C(1, 20, 36, data::DBE::even))/(O2.sum_C(1, 19, 35, data::DBE::odd)) << "\n";
-            std::cout << "OEP (FA): " << (O2.C(1, 22) + 6.0*O2.C(1, 28) + O2.C(1, 26))/(4*O2.C(1, 23) + 4*O2.C(1, 25)) << "\n";
-            std::cout << "TAR (FA) Par: " << (O2.sum_C(1, {24, 26, 28}))/(O2.sum_C(1, {12, 14, 16})) << "\n";
-            std::cout << "TAR (FA) Impar: " << (O2.sum_C(1, {27, 29, 31}))/(O2.sum_C(1, {15, 17, 19})) << "\n";
-            std::cout << "C36 Indice de Acido Hepanoico: " << O2.C(6, 36)/O2.C(6, 31) << "\n";
-            std::cout << "Hopano / Acido Estanoico: " << O2.C(6, 31)/O2.C(5, 28) << "\n";
-            std::cout << "Razao Rocha 1: " << O2.sum_dbe(1, 7)/(O2.sum_dbe(1, 7) + O2.sum_dbe(8, 25)) << "\n";
-            std::cout << "Razao Rocha 2: " << O1.sum_dbe(4, 10)/(O1.sum_dbe(4, 10) + O1.sum_dbe(11, 23)) << "\n";
-            std::cout << "Razao Rocha 3: " << NO.get_DBE(10)->intensity << "\n";
-            std::cout << "Razao Rocha 4: " << O1.get_DBE(4)->intensity << "\n";
-            std::cout << "Razao Rocha 5: " << N.sum_dbe(6, 10)/(N.sum_dbe(6, 10) + N.sum_dbe(10, 27)) << "\n";
-            std::cout << "Razao Rocha 6: " << N.sum_dbe(2, 14)/(N.sum_dbe(2, 14) + N.sum_dbe(15, 29)) << "\n";
-            std::cout << "Razao Rocha 7: " << O1.sum_dbe(4, 12) + O2.get_DBE(12)->intensity/(O1.sum_dbe({4, 12, 14, 16}) + O2.sum_dbe({12, 13})) << "\n";
-            std::cout << "Razao Rocha 8: " << N.sum_dbe({8, 9}) + NO.sum_dbe({9, 10})/(N.sum_dbe({8, 9, 13, 16}) + NO.sum_dbe({9, 10, 19, 20})) << "\n";
+            input.print_paleoenvironment(std::cout);
+            input.print_paleoenvironment(file);
         }
         if(opt == '3'){
-            auto N = *input.get_class("N");
-            std::cout << "DBE9  da Classe N: " << N.get_DBE(9)->intensity << "\n";
-            std::cout << "DBE12 da Classe N: " << N.get_DBE(12)->intensity << "\n";
-            std::cout << "DBE15 da Classe N: " << N.get_DBE(15)->intensity << "\n";
+            input.print_maturity(std::cout);
+            input.print_maturity(file);
         }
     }
 }
@@ -173,13 +120,13 @@ int main(int argc, char **argv){
 
         switch(opt){
             case '1':
-                relative_abundancy(input, &new_file);
+                relative_abundancy(input, new_file);
                 break;
             case '2':
-                petrochemical_study(input, &new_file);
+                petrochemical_study(input, new_file);
                 break;
             case '3':
-                geochemical_study(input, &new_file);
+                geochemical_study(input, new_file);
                 break;
         }
     }
